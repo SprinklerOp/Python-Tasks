2bc38fb92cae4b57c9a7f81ddd1c5826e20abb00
SprinklerOp <pdattaram83@gmail.com>
2024-06-24 01:45:24 +0530

    my commit

--- sm task.py
+++ sm task.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 def send_sms_message(message):
-    account_sid = 'ACc92aec7d8fdf0a995c21bb6ed0f9e287'
-    auth_token = '1e272c02402fe7b9c2dd703155a74411'
+    account_sid = ''
+    auth_token = ''
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body=message,
